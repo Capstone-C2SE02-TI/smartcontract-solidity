2a9e2b99d29ab38934296fadcf69dd6aff2ea75e
Nguyen Hong Lich <lichnguyenhong.it@gmail.com>
2022-09-10 09:50:34 +0700

    feat: buy

--- contracts/DEX.sol
+++ contracts/DEX.sol
@@ -7,13 +7,48 @@ import "./ERC20.sol";
 
 
 contract DEX{
-    event Bought(uint256 amount);
-    event Sold(uint256 amount);
+    event Bought(address payable buyer, uint256 amount);
+    event Sold(address payable seller, uint256 amount);
+    
+    uint256 price;
+    address payable owner;
+    ERC20 token;
 
-    IERC20 token;
+
+    constructor(address _token, uint256 _price){
+        token = ERC20(_token);
+        price = _price;
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,'Only owner');
+        _;
+
+    }
+
+    receive() payable external{
+        
+    }
 
 
-    constructor(address _token){
-        token = IERC20(_token);
+    function buy(uint256 amount) payable public{
+        require(amount * price == msg.value,"Not send exactly ETH");
+        require(token.balanceOf(address(this)) >= amount, "Don't Enough token to swap");
+
+        (bool success,) = address(token).call(
+            abi.encodeWithSelector(ERC20.transfer.selector,msg.sender,amount)
+            
+        );
+
+        require(success,"Revert");
+    }
+
+    function updateToken(address _newToken) public onlyOwner{
+        token = ERC20(_newToken);
+    }
+
+    function updateOwner(address _newOwner) public onlyOwner{
+        owner = payable(_newOwner);
     }
 }
--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -37,7 +37,7 @@ interface IERC20 {
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
-contract ERC20Token is IERC20 {
+contract ERC20 is IERC20 {
 
     uint256 public totalSupply;
     uint256 constant private MAX_UINT256 = 2**256 - 1;
